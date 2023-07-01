bea0951b682f77ec01aae08e1ed81c65904826da
ka11den <ka1den@mail.ru>
2023-07-01 18:19:00 +0400

    fix: adaptiva small device, header animation; feat: validate form

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,82 @@ navLinks.forEach(n => n.addEventListener('click', () => {
 }))
 
 // payment
+const homeForm = document.getElementById('home-form')
+const homeSteamid = document.getElementById('home-input-steamid')
+const homeRadio = document.getElementById('contactChoice3')
+const homeSum = document.getElementById('home-input-sum')
+const homeMessage = document.getElementById('home-message')
+
+const paymentForm = async (e) => {
+  e.preventDefault();
+
+  if (homeSteamid.value === '') {    
+    // error
+    homeMessage.classList.add('color-red');
+    homeMessage.textContent = "Вы не заполнили SteamId!"
+
+    // clear error
+    setTimeout(() => {
+      // error
+      homeMessage.classList.remove('color-red');
+      homeMessage.textContent = ""
+    }, 3000);  
+
+    return;
+  }
+
+  if (homeSum.value === '') {
+    // error
+    homeMessage.classList.add('color-red');
+    homeMessage.textContent = "Вы не заполнили сумму!"
+
+    // clear error
+    setTimeout(() => {
+      // error
+      homeMessage.classList.remove('color-red');
+      homeMessage.textContent = ""
+    }, 3000);  
+
+    return;
+  }
+
+  if (homeRadio.checked === false) {
+    // error
+    homeMessage.classList.add('color-red');
+    homeMessage.textContent = "Вы не поставили галочку!"
+
+    // clear error
+    setTimeout(() => {
+      // error
+      homeMessage.classList.remove('color-red');
+      homeMessage.textContent = ""
+    }, 3000);  
+
+    return;
+  }
+
+  homeMessage.classList.add('color-green');
+  homeMessage.textContent = "Форма успешно заполнена"
+
+  // clear error
+  setTimeout(async () => {
+    // error
+    homeMessage.classList.remove('color-green');
+    homeMessage.textContent = ""
+      const data = {
+        amount: "3232",
+        shop_id: "4232",
+      }
+  
+    await fetch("https://cardlink.link/api/v1/bill/create", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(window.location.href = "http://www.example.com");
+  }, 3000);
+  
+}
+
+homeForm.addEventListener('submit', paymentForm)
